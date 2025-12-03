3d893d8e3d6134607af4ff0f840c3db96d7c1594
Sebas <sebastian11medrano@gmail.com>
2025-12-03 23:11:34 +0000

    se añadio el diagrama de componentes

---
+++ frontend/src/pages/artists/Admin.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { Button } from "flowbite-svelte";
+</script>
+
+<div class="container mx-auto p-4">
+    <Button outline>Default</Button>
+    <Button outline color="dark">Dark</Button>
+    <Button outline color="green">Green</Button>
+    <Button outline color="red">Red</Button>
+    <Button outline color="yellow">Yellow</Button>
+    <Button outline color="purple">Purple</Button>
+</div>
