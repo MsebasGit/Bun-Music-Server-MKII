da21461e9918442033adfe527a9c828120c244d9
Sebas <sebastian11medrano@gmail.com>
2025-12-06 06:11:32 +0000

    chore: Remove old backend files from root directory

--- src/utilities/correctDate.ts
+++
@@ -1,5 +0,0 @@
-export function toCorrectDate(fecha:string) :string{
-  const fechStr = fecha!.toString()
-  const fecIns = fechStr!.split('-')
-  return `${fecIns[2]}-${fecIns[1]}-${fecIns[0]}`;
-}
