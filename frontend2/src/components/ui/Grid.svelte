c1c7edcf9d00a7d494a8e68bc9b93b0ed38127b9
sebas <sebastian11medrano@gmail.com>
2025-12-12 22:46:03 +0000

    Corrección de delete en playlist

--- frontend2/src/components/ui/Grid.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    export let items: any[] = [];
-</script>
-
-<div class="grid gap-4 md:gap-6 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6">
-    {#each items as item (item.id ?? item.id_song ?? item.id_album ?? item.id_artist ?? item.id_playlist)}
-        <slot {item} />
-    {/each}
-</div>
